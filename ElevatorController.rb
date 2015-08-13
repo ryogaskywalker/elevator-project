cfd0db2962a4cc73245548125d03bba7909cdcc4
奥西亮賀 <okunishiryoga@okunishuganoMBP.lan>
2015-08-13 23:01:48 +0900

    filewrite is done

--- ElevatorController.rb
+++ ElevatorController.rb
@@ -1,20 +1,35 @@
 #!/bin/ruby
+require 'csv'
 
 LOOP_NUM = 10000
 
 # global variables
 $CAPACITY = 20
-# 2m/s (ref:README.md)
-$SPEED = 2
+# 2.2 m / s (ref:README.md)
+$SPEED = 2.2
+# 10 / s
+$INCIDENCE = 10
 
+FILENAME = "./test.csv"
 
 class ElevatorController
     def simulate
-        for i in 1..LOOPNUM
+        for time in 1..LOOP_NUM
             # write Elevator Object Class
+            # output file
+            CSV.open(FILENAME, "a", :encoding => "SJIS") do |writer|
+                #writer << [time, eObj]
+                writer << [time]
+            end
+        end
+    end
+    def judge
+        for index in eObj do
+            # call Elevator Objects method
+            
         end
     end
 end
 
-sim = ElavatorController.new()
+sim = ElevatorController.new()
 sim.simulate
