ddc46457b1f36045ab0568bddfadbc09484e77a0
奥西亮賀 <okunishiryoga@okunishuganoMBP.lan>
2015-08-13 22:32:01 +0900

    add elevatorcontroller.rb and readme.md

---
+++ ElevatorController.rb
@@ -0,0 +1,20 @@
+#!/bin/ruby
+
+LOOP_NUM = 10000
+
+# global variables
+$CAPACITY = 20
+# 2m/s (ref:README.md)
+$SPEED = 2
+
+
+class ElevatorController
+    def simulate
+        for i in 1..LOOPNUM
+            # write Elevator Object Class
+        end
+    end
+end
+
+sim = ElavatorController.new()
+sim.simulate
