7406bed8bbdde405b8bf1a4a86455818ad889637
yuri-horita <yuri_horita@r.recruit.co.jp>
2015-08-19 20:46:49 +0900

    implemented hO and fO

--- FloorObject.rb
+++ FloorObject.rb
@@ -1,3 +1,51 @@
 #!/bin/ruby
+require 'csv'
+require './Parameters.rb'
+FILENAME = "./floor_object_log.csv"
+
 class FloorObject
+  def initialize(floornum)
+    @floor_number = floornum # expecting 23 or 33~41
+    @human_arr = Array.new() # array list of human wating elevator in this floor
+    @goup_flg = false # true: someone wants to go up
+    @godown_flg = false # true: someone wants to go down
+  end
+
+  def set_human(human_num)
+    # don't care human_num is minus number case
+    for num in 1..human_num do
+      @human_arr<<HumanObject.new(@floor_number)
+    end
+    update_goup_flg
+    update_godown_flg
+  end
+
+  def update_goup_flg
+    # arr内のhOを舐めて上に行きたい人がいればtrueに
+    # いなければfalseに
+    # 毎回走査するイメージ
+    @goup_flg = false
+    for human in @human_arr do
+      if human.ans_going_up(@floor_number) then
+        @goup_flg = true
+      end
+    end
+  end
+
+  def update_godown_flg
+    # same as above
+    @godown_flg = false
+    for human in @human_arr do
+      unless human.ans_going_up(@floor_number) then
+        @godown_flg = true
+      end
+    end
+  end
+
+  def log
+    CSV.open(FILENAME, "a", :encoding => "SJIS") do |writer|
+      writer << [$time, @human_arr.length]
+    end
+  end
+
 end
--- HumanObject.rb
+++ HumanObject.rb
@@ -1,6 +1,29 @@
 #!/bin/ruby
 class HumanObject
-  def initialize(destfloor)
-    @destination_floor = destfloor
+  def initialize(current_floor)
+    @destination_floor = generate_destfloor_randomly(current_floor)
+  end
+
+  def generate_destfloor_randomly(current_floor)
+    dest_floor = rand(10)+32 # generate integer of [32,41]
+    if dest_floor = 32 then
+      dest_floor = 23 # floor#23 is irregular case
+    end
+    unless dest_floor = current_floor then
+      return dest_floor
+    else # dest=current case(unfortunately)
+      return generate_destfloor_randomly(current_floor) # recursive call
+    end
+  end
+
+  def ans_going_up(current_floor)
+    # if this human wants to go up, return true
+    # else, return false
+    # (don't care dest=current situation)
+    if @destination_floor > current_floor then
+      return true
+    else
+      return false
+    end
   end
 end
