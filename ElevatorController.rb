425e9ca9f88ae5c1b503154826b262b613cb7a1a
奥西亮賀 <okunishiryoga@okunishiryouga-no-MacBook-Pro.local>
2015-08-20 20:31:07 +0900

    add controller

--- ElevatorController.rb
+++ ElevatorController.rb
@@ -1,24 +1,52 @@
 #!/bin/ruby
 require 'csv'
 require './Parameters.rb'
+require './ElevatorObject.rb'
+require './FloorObject.rb'
 
-FILENAME = "./test.csv"
+FILENAME_MAIN = "./main_log.csv"
+
 class ElevatorController
+    def initialize
+        @eO = []
+        @fO = []
+        for index in 1..7 
+            @eO.push(ElevatorObject.new(23))
+        end
+        for index in 23..41
+            if index < 24 & index >= 33 then
+                @fO.push(FloorObject.new(index))
+            end
+        end
+    end
     def simulate
         for $time in 1..LOOP_NUM
             # write Elevator Object Class
+            @eO.each_with_index {
+                
+            }
+            @fO.each_with_index { |fObj, index|
+                if index == 0 then
+                    fObj.set_human($INCIDENCE)
+                elsif index == 1 then
+                    fObj.set_human(5)
+                elsif rand(600) == 1 then
+                    fObj.set_human(1)
+                end
+            }
             # output file
-            CSV.open(FILENAME, "a", :encoding => "SJIS") do |writer|
+            CSV.open(FILENAME_MAIN, "a", :encoding => "SJIS") do |writer|
                 #writer << [time, eObj]
-                writer << [$time]
+                writer << [$time, @eO[0].]
             end
         end
     end
     def judge
-        for index in eObj do
+        for index in @eO do
             # call Elevator Objects method
             
         end
+        for index in @fO do
     end
 end
 
