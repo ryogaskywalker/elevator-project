56669beb6e67d6fb23de63a9b040af56a5d1f5f4
nao-akechi <nao_akechi@r.recruit.co.jp>
2015-08-23 21:04:22 +0900

    Updated ElevatorObject.rb

--- ElevatorObject.rb
+++ ElevatorObject.rb
@@ -2,14 +2,19 @@
 require 'csv'
 require './Parameters.rb'
 
-FILENAME = "./elevator_object_log.csv"
+FILENAME_ELEVATOR = "./elevator_object_log.csv"
 
 class ElevatorObject
+  attr_accessor :state
+  attr_accessor :current_floor
+
   def initialize(current_floor)
     @state = "stay" # stay, up or down
-    @destination_floor_arr = Array.new # array of elevator's destination
     @current_floor = current_floor
+    @destination_floor_arr = Array.new # array of elevator's destination
     @human_arr = Array.new # array of human in the elevator
+    @move_time = 0
+    @stop_flag = false # when get on or get off
   end
 
   # get on the elevator method
@@ -17,7 +22,7 @@ class ElevatorObject
   def get_on(human_arr)
     @human_arr.concat(human_arr)
     human_arr.each {|human|
-      destination_floor = human.destination_floor # TODO using HumanObject getter method?
+      destination_floor = human.destination_floor
       unless destination_floor_arr.include?(destination_floor) then
         destination_floor_arr.push(destination_floor)
       end
@@ -29,16 +34,53 @@ class ElevatorObject
   def get_off
     new_human_arr = Array.new
     @human_arr.each {|human|
-      destination_floor = human.destination_floor # TODO using HumanObject getter method?
+      destination_floor = human.destination_floor
       unless destination_floor == @current_floor then
         new_human_arr.push(human)
       end
     }
     @human_arr = new_human_arr
+    if @human_arr.length == 0 then
+      @state = "stay"
+    end
+  end
+
+  def check_move(time, sign)
+    if @move_time == time then
+      @current_floor += sign * 1
+      @move_time = 0
+      if @destination_floor_arr.include?(@current_floor) then
+        @destination_floor_arr.delete(@current_floor)
+        get_off()
+        @stop_flag = true
+      end
+      return false
+    end
+    return true
+  end
+
+  def move
+    increment_flag = true
+    if @stop_flag then
+      if @move_time == $stop_time then
+        @stop_flag = false
+        @move_time = 0
+        increment_flag = false
+      end
+    elsif @state == "up" then
+      time = @current_floor = 23 ? $move_time_long : $move_time_short
+      increment_flag = check_move(time, 1)
+    elsif @state == "down" then
+      time = @current_floor = 33 ? $move_time_long : $move_time_short
+      increment_flag = check_move(time, -1)
+    end
+    if increment_flag then
+      @move_time += 1
+    end
   end
 
   def log
-    CSV.open(FILENAME, "a", :encoding => "SJIS") do |writer|
+    CSV.open(FILENAME_ELEVATOR, "a", :encoding => "SJIS") do |writer|
       writer << [$time, @human_arr.length]
     end
   end
